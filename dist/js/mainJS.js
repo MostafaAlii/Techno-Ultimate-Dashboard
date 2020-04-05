f85114d70d19ca08dd321e9a607b075b537d5bf8
MostafaAlii <mostafa0alii@gmail.com>
2020-04-05 17:00:21 +0200

    Lesson 18 - Add Fullscreen Icon With JS

--- dist/js/mainJS.js
+++ dist/js/mainJS.js
@@ -13,16 +13,63 @@ $(function (){
         railalign: 'left',
     });
     
-    // Sidebar & Content bar Toggle
-    $('.toggle-sidebar').on('click', function (){
-        $('.content-area, .sidebar').toggleClass('no-sidebar');
-    });
-
     // Toggle Submenu
     $(".toggle-submenu").on("click", function(){
         $(this).find(".fa-caret-left").toggleClass("down");
         $(this).next(".child-links").slideToggle();
     });
-});
+
+    // Sidebar & Content bar Toggle
+    $('.toggle-sidebar').on('click', function (){
+        $('.content-area, .sidebar').toggleClass('no-sidebar');
+        // If Explain هشوف شرح ليها عشان فى تركاية هنا
+    });
+    
+    // Fullscreen Changing Btn
+    $(".toggle-fullscreen").on("click", function() {
+        $(this).toggleClass('full-screen');
+            if ($(this).hasClass("full-screen")) {
+                // Page Is Now Full Screen
+                openFullscreen();
+            } else {
+                // Page is Not Full Screen
+                closeFullscreen();
+            }
+        });
+    });
+    // Open FullScreen
+    var elem = document.documentElement;
+    function openFullscreen() {
+        if (elem.requestFullscreen) {
+          elem.requestFullscreen();
+        } else if (elem.mozRequestFullScreen) { /* Firefox browser */
+          /* Firefox */
+          elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullscreen) { /* Chrome ,Safari and Opera browser */
+          /* Chrome, Safari and Opera */
+          elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE browser */
+          /* IE/Edge */
+          elem.msRequestFullscreen();
+        }
+      }
+      // Close FullScreen
+      function closeFullscreen() {
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+          /* Firefox */
+          document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+          /* Chrome, Safari and Opera */
+          document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+          /* IE/Edge */
+          document.msExitFullscreen();
+        }
+      }
+    
+
+    
 const mySet = new Set();
 const myMap = new Map();
